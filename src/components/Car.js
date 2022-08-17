8cc30e141646e19ae4fc0b67f21cf0468b9ee942
CalebAurand <caurand13@gmail.com>
2022-08-16 20:27:57 -0500

    added container folder, container files for Home, Dashboard, and Car components, and global state to these components

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Container, Paper, Chip } from '@mui/material';
-import cars from '../cars.json' // remove this
 
 const Car = (props) => {
     const id = props.match.params.id
-    {/* Change cars to props.cars and remove the cars.json import above */}
-    const car = cars.find(c => c.id == id)
-
+    const car = props.cars.find(c => c.id == id)
+    console.log("car", car);
     return (
         <Container maxWidth="sm" className="car-container">
             <Paper className="car-paper">
